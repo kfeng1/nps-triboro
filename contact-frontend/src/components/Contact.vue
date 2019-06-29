4a1b5ba0b8bd77b56a4c8fb47ba8361b0f205c2e
Kylin Feng <kylin.feng@berkeley.edu>
2019-06-29 13:12:10 -0700

    first commit

---
+++ contact-frontend/src/components/Contact.vue
@@ -0,0 +1,27 @@
+<template>
+    <tr>
+        <td>
+            {{contact.name}}
+        </td>
+        <td>
+            {{contact.email}}
+        </td>
+    </tr>
+</template>
+
+<script>
+    export default {
+        data() {
+
+        },
+        props: ['contact']
+
+    }
+</script>
+
+
+<style>
+
+</style>
+
+
---
+++ contact-frontend/src/components/ContactTable.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <table class="table">
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Last Contact Date</th>
+                <th>Next Contact Date </th>
+                <th>Contact Priority</th>
+            </tr>
+            </thead>
+            <tbody v-for="contact in contacts">
+                <contact :contact = "contact"></contact>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    import Contact from './Contact.vue'
+    export default{
+        data(){
+           return{
+                contacts: [{name: "test1", email: "a"}, {name:"test2", email:"b"}]
+           }
+        },
+        methods: {
+            tableSort(){
+
+            }
+        },
+        components: {
+            Contact
+        }
+    }
+</script>
